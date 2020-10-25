5e19260f0d9f4eae08fbf20b672e43438900fd92
lakyoungshin <s2fkruds2@gmail.com>
2020-10-25 11:25:56 +0900

    lakyoungshin

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,70 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``, *실제로 인스턴스에서 템플릿을 쓰지는 않음
+    methods: {
+
+    },
+    components: { }, 
+    computed: {
+
+    },
+    watch: {
+
+    },
+    // 주로 쓰는 메쏘드 
+    mounted: function() {
+        console.log("mounted");
+    },
+    updated: function() {
+        console.log("updated");
+    },
+});
+Vue.component("aa", { 
+/* pdtmc^2w */
+    
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
+
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
